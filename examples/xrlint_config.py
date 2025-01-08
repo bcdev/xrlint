73c696072ae6503247dbdf3dfe0f2c17dca5286f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-01-08 12:19:35 +0100

    improved example

--- examples/xrlint_config.py
+++ examples/xrlint_config.py
@@ -1,37 +1,38 @@
 # XRLint configuration file example
 
 from xrlint.config import Config
-from xrlint.rule import RuleContext
-from xrlint.rule import RuleOp
+from xrlint.node import DatasetNode
 from xrlint.plugin import Plugin
 from xrlint.plugin import PluginMeta
-from xrlint.node import DatasetNode
+from xrlint.rule import RuleContext
+from xrlint.rule import RuleOp
 
 
-GOOD_TITLE = "Hello World!"
-
-plugin = Plugin(meta=PluginMeta(name="hello-plugin", version="1.0.0"))
+plugin = Plugin(
+    meta=PluginMeta(name="hello-plugin", version="1.0.0"),
+    configs={
+        "recommended": Config.from_value(
+            {
+                "rules": {
+                    "hello/good-title": "error",
+                },
+            }
+        )
+    },
+)
 
 
 @plugin.define_rule(
-    "good-title", description=f"Dataset title should be {GOOD_TITLE!r}."
+    "good-title", description=f"Dataset title should be 'Hello World!'."
 )
-class TitleHello(RuleOp):
+class GoodTitle(RuleOp):
     def dataset(self, ctx: RuleContext, node: DatasetNode):
-        if node.dataset.attrs.get("title") != GOOD_TITLE:
+        good_title = "Hello World!"
+        if node.dataset.attrs.get("title") != good_title:
             ctx.report(
                 "Attribute 'title' wrong.",
-                suggestions=[f"Rename it to {GOOD_TITLE!r}."],
+                suggestions=[f"Rename it to {good_title!r}."],
             )
-
-
-plugin.configs["recommended"] = Config.from_value(
-    {
-        "rules": {
-            "hello/good-title": "error",
-        },
-    }
-)
 
 
 def export_configs():
@@ -44,5 +45,6 @@ def export_configs():
                 "hello": plugin,
             },
         },
-        plugin.configs["recommended"],
+        "recommended",
+        "hello/recommended",
     ]
