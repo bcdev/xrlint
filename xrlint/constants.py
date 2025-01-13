ba2ea3d61aeda03d4a3df7e3701360f6c2b26856
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-01-13 12:49:45 +0100

    using Final

--- xrlint/constants.py
+++ xrlint/constants.py
@@ -1,18 +1,22 @@
-SEVERITY_ERROR = 2
-SEVERITY_WARN = 1
-SEVERITY_OFF = 0
+from typing import Final
 
-SEVERITY_NAME_TO_CODE = {
+SEVERITY_ERROR: Final = 2
+SEVERITY_WARN: Final = 1
+SEVERITY_OFF: Final = 0
+
+SEVERITY_NAME_TO_CODE: Final = {
     "error": SEVERITY_ERROR,
     "warn": SEVERITY_WARN,
     "off": SEVERITY_OFF,
 }
-SEVERITY_CODE_TO_NAME = {v: k for k, v in SEVERITY_NAME_TO_CODE.items()}
-SEVERITY_CODE_TO_CODE = {v: v for v in SEVERITY_NAME_TO_CODE.values()}
+SEVERITY_CODE_TO_NAME: Final = {v: k for k, v in SEVERITY_NAME_TO_CODE.items()}
+SEVERITY_CODE_TO_CODE: Final = {v: v for v in SEVERITY_NAME_TO_CODE.values()}
 
-SEVERITY_ENUM: dict[int | str, int] = SEVERITY_NAME_TO_CODE | SEVERITY_CODE_TO_CODE
-SEVERITY_ENUM_TEXT = ", ".join(f"{k!r}" for k in SEVERITY_ENUM.keys())
+SEVERITY_ENUM: Final[dict[int | str, int]] = (
+    SEVERITY_NAME_TO_CODE | SEVERITY_CODE_TO_CODE
+)
+SEVERITY_ENUM_TEXT: Final = ", ".join(f"{k!r}" for k in SEVERITY_ENUM.keys())
 
-MISSING_DATASET_FILE_PATH = "<dataset>"
+MISSING_DATASET_FILE_PATH: Final = "<dataset>"
 
-CORE_PLUGIN_NAME = "__core__"
+CORE_PLUGIN_NAME: Final = "__core__"
