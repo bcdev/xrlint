5db6ee5ffdb6791d4679bcea868d6a95b1f0685a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-01-20 18:37:57 +0100

    experimenting with ml-dataset config

--- xrlint/plugins/xcube/__init__.py
+++ xrlint/plugins/xcube/__init__.py
@@ -7,11 +7,12 @@ def export_plugin() -> Plugin:
     from .plugin import plugin
 
     import_submodules("xrlint.plugins.xcube.rules")
-    # import_submodules("xrlint.plugins.xcube.processors")
+    import_submodules("xrlint.plugins.xcube.processors")
 
     plugin.configs["recommended"] = Config.from_value(
         {
             "name": "xcube-recommended",
+            "ignores": ["**/*.levels"],
             "plugins": {
                 "xcube": plugin,
             },
@@ -30,10 +31,19 @@ def export_plugin() -> Plugin:
     plugin.configs["all"] = Config.from_value(
         {
             "name": "xcube-all",
+            "ignores": ["**/*.levels"],
             "plugins": {
                 "xcube": plugin,
             },
             "rules": {f"xcube/{rule_id}": "error" for rule_id in plugin.rules.keys()},
+        }
+    )
+    plugin.configs["multi-level-datasets"] = Config.from_value(
+        {
+            "name": "xcube-multi-level-datasets",
+            "files": ["**/*.levels"],
+            "ignores": ["**/*.zarr", "**/*.nc"],
+            "processor": "xcube/multi-level-dataset",
         }
     )
 
--- xrlint/plugins/xcube/processors/mldataset.py
+++ xrlint/plugins/xcube/processors/mldataset.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Any
 
 import xarray as xr
@@ -15,15 +16,28 @@ class MultiLevelDatasetProcessor(ProcessorOp):
         self, file_path: str, opener_options: dict[str, Any]
     ) -> list[tuple[xr.Dataset, str]]:
         engine = opener_options.pop("engine", "zarr")
-        new_file_path = file_path + "/" + "0.zarr"
-        return [
-            (
-                xr.open_dataset(new_file_path, engine=engine, **opener_options),
-                new_file_path,
-            )
-        ]
+
+        level_datasets = []
+        for level in itertools.count():
+            level_path = f"{file_path}/{level}.zarr"
+            try:
+                level_dataset = xr.open_dataset(
+                    level_path, engine=engine, **opener_options
+                )
+            except FileNotFoundError:
+                break
+            level_datasets.append((level_dataset, level_path))
+
+        for level, (level_dataset, level_path) in enumerate(level_datasets):
+            level_dataset.attrs["_LEVEL_INFO"] = {
+                "index": level,
+                "count": len(level_datasets),
+                "datasets": [ds for ds, _ in level_datasets],
+            }
+
+        return level_datasets
 
     def postprocess(
         self, messages: list[list[Message]], file_path: str
     ) -> list[Message]:
-        return messages[0]
+        return list(itertools.chain(*messages))
